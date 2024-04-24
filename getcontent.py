b0ee368647c0f8a278ddb231fb2d4f5611ab96f0
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-04-24 13:37:23 +1200

    Changed to Chrome webdriver, and made headless

--- getcontent.py
+++ getcontent.py
@@ -10,6 +10,11 @@ from datetime import date
 import os
 import time
 import requests
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 """Important Notes:
 You will need to have the Microsoft edge Webdriver installed (and added to PATH) before this will work.
@@ -25,8 +30,13 @@ def lawa_download_link_groundwater():
     """This Function goes to the lawa downloads page, clicks download, then graps the download link of the 
     confirm button (this makes sure we have the latest dataset). It returns a string url"""
     #Currently this is hardcoded to look for the Groundwater quality dataset on the lawa downloads page
-    driver = webdriver.Edge()
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--headless=new")
+    
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     driver.get(lawa_url)
+
     ground_water_xpath = """//*[@id="body"]/div[2]/div/div/div[4]/div[1]/div/a"""
     driver.find_element(By.XPATH, ground_water_xpath).click()
     confirm_xpath = """//*[@id="download-data-gwquality-dataset"]/div[1]/div/a"""
@@ -41,27 +51,22 @@ def lawa_download_link_groundwater():
 
 
 
-def download_excel_workbook(url):
+def download_excel_workbook(url, name="Groundwater"):
     """Function that takes the url download link of a file, and then returns a dictionary of worksheet:pandas df pairs"""
     print(f"Downloading content from {url}")
     response = requests.get(url)
     content_type = response.headers.get('Content-Type')
     assert 'excel' in content_type or url.endswith(('.xls', '.xlsx'))
-    file_path = "Lawa_ground_water-" + str(date.today()) + ".xlsx"
+    file_path = name + str(date.today()) + ".xlsx"
     with open(file_path, 'wb') as f: #Its safer to write the dataset to disk first, then read it into pandas
                 f.write(response.content)
-                df = pd.read_excel(file_path)
     print("File written")
     dfs = pd.read_excel(file_path, sheet_name=None)
 
     return dfs
-        
-    
 
 dataset_link = lawa_download_link_groundwater()
 
-dfs = download_excel_workbook(dataset_link)
-
-for sheet_name, df in dfs.items(): # dfs is a dictionary of workbook:pandas.df pairs
-    print(f"Sheet name: {sheet_name}")
-    print(df.head()) 
+# dfs = download_excel_workbook(dataset_link)
+# print(type(dfs))
+# print(dfs)
