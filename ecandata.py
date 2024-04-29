7527ead0c50b499887c456033a4925d79f28b67d
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-04-29 15:35:25 +1200

    Fixed up the code repetition and added comments

--- ecandata.py
+++ ecandata.py
@@ -11,6 +11,7 @@ from graphql import build_client_schema, GraphQLSchema, print_schema, parse, exe
 url = "https://gis.ecan.govt.nz/arcgis/rest/services/Public/Groundwater/MapServer/19/query?outFields=*&where=1%3D1&f=geojson"
 
 def get_ecan_data():
+    """Function to call the Ecan api. Returns a pandas dataframe"""
     response = requests.get(url)
     data = response.json()
 
@@ -26,20 +27,7 @@ def get_ecan_data():
             'WGS84_LATITUDE': latitude
         })
     df = pd.DataFrame(extracted_data)
-
-
-    extracted_data = []
-    for feature in data['features']:
-        properties = feature['properties']
-        longitude, latitude = feature['geometry']['coordinates']
-        extracted_data.append({
-            'ID': properties['ID'],
-            'Well_No': properties['Well_No'],
-            'Primary_Use': properties['Primary_Use'],
-            'WGS84_LONGITUDE': longitude,
-            'WGS84_LATITUDE': latitude
-        })
-
-    df = pd.DataFrame(extracted_data)
-    #print(df)
     return df
+
+
+print(get_ecan_data())
