60654f4b265a45ce66e0416cb581c6e5893231d4
jake1441 <jacob@sysint.co.nz>
2024-05-06 15:19:25 +1200

    resolve selenium issues

--- docker configs/docker_selenium/run_docker_scraper.sh
+++ docker configs/docker_selenium/run_docker_scraper.sh
@@ -6,14 +6,15 @@
 # name of the image is selenium-headless-d472
 # the docker container name is selcontainer
 
-fname="getcontent.py"
+fname="ecanmain.py"
 docker rmi selenium-headless-d472
 docker build . -t selenium-headless-d472
 docker run -d --name selcontainer selenium-headless-d472
-docker cp $fname selcontainer:/python_scrape
-docker exec -it selcontainer python $fname
+docker cp scripts/ selcontainer:/python_scrape/
+docker exec -it selcontainer python /python_scrape/scripts/$fname
 # copy file out of docker container (may need to be a for loop if theres multiple files as not tested!)
-docker cp selcontainer:/python_scrape/$(docker exec -i selcontainer find . -name *.xlsx | sed 's/^.\///') .
+#docker cp selcontainer:/python_scrape/$(docker exec -i selcontainer find . -name *.xlsx | sed 's/^.\///') .
+docker cp selcontainer:/python_scrape/ docker_output/
 # stop container and remove.
 docker stop selcontainer
 docker rm selcontainer
