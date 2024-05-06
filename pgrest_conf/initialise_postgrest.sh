43a2055d6704a4b5ab20a45031ec08cfd929f359
jacob reid <jacob@sysint.co.nz>
2024-05-06 18:32:17 +1200

    changes to postgrest sql scripts

--- pgrest_conf/initialise_postgrest.sh
+++ pgrest_conf/initialise_postgrest.sh
@@ -9,24 +9,22 @@
 # this and the next line sudo docker run --name tutorial to the preferred name of your container.
 # did you check that tutorial was not named after another container you had?
 
+# collect postgrest application
+wget https://github.com/PostgREST/postgrest/releases/download/v12.0.2/postgrest-v12.0.2-linux-static-x64.tar.xz postgrest-v12.0.2-linux-static-x64.tar.xz
+tar xJf postgrest-v12.0.2-linux-static-x64.tar.xz
+
 docker stop tutorial
 # this command should only be run if you are confident you don't care about other stopped containers being deleted!
 # docker ps -aq shows all containers but only ids. docker ps -a usually only shows running containers.
 docker rm $(docker ps -aq)
+database_user=data_user
 
-if [ -x "$(command -v docker)" ]; then
-	echo "Docker installed! making sure docker is started!"
-	sudo service docker start
-	else
-		echo "docker not installed!"
-		exit
-fi
 # this command creates the tutorial posgres docker
 sudo docker run --name tutorial -p 5433:5432 \
                 -e POSTGRES_PASSWORD=mysecretpassword \
                 -d postgres
-echo "Waiting 10 seconds for postgres to start!"
-sleep 10s
+
+sleep 5s
 #  sudo docker exec -ti tutorial psql -U postgres 
 # test create schema
 cat << EOF | sudo docker exec -i tutorial psql -U postgres 
@@ -73,10 +71,10 @@ grant web_dbaccess to authenticator;
 EOF
 
 # creating the user who will manipulate the data using CURL
-# this role is used by JWT.
+# this role is used by JWT. Payload { "role" : "data_user" }
 cat << EOF | sudo docker exec -i tutorial psql -U postgres 
-create role data_user nologin;
-grant data_user to authenticator;
+create role $database_user nologin;
+grant $database_user to authenticator;
 
 grant usage on schema datab1 to data_user;
 grant all on datab1.data to data_user;
@@ -126,6 +124,4 @@ echo "jwt-secret = \"$(LC_ALL=C tr -dc 'A-Za-z0-9' </dev/urandom | head -c32)\""
 # Running postgrest installed from a package manager
 # when happy uncomment this line below.
 # postgrest tutorial.conf
-wget https://github.com/PostgREST/postgrest/releases/download/v12.0.2/postgrest-v12.0.2-linux-static-x64.tar.xz -O postgrest-v12.0.2-linux-static-x64.tar.xz
-tar -xvfpostgrest-v12.0.2-linux-static-x64.tar.xz
 sh start_postgres.sh
