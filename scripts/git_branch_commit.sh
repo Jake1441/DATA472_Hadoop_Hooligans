c9c6092bee422065f19b422c740a9d7da34b4fda
jacob reid <jacob@sysint.co.nz>
2024-05-07 21:31:42 +1200

    update github script for branching

--- scripts/git_branch_commit.sh
+++ scripts/git_branch_commit.sh
@@ -1,16 +1,21 @@
 # helpful git script.
+working_dir=$(pwd)
 
 # set up parameters
-new_commit_message='test github branching'
+new_commit_message='update github script for branching'
 
 # this is your branch you want to branch off, think of it like a node
 current_branch=develop
 git switch $current_branch
 
 # this is the leaf branch node -> leaf node
-new_branch=git_test
+new_branch=git_script_fix_adding
 git branch $new_branch
 git checkout $new_branch
+
+# when changing need to go back to root git.
+
+cd "$working_dir"
 git add .
 # git commit -m "$new_commit_message"
 
