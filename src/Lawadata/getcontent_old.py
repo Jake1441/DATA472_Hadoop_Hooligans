d624708116b8e9b1e3015e8c6fc696d96ffbeac7
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-19 17:56:58 +1200

    Decomposed and fixed lawa download scripts

--- src/Lawadata/getcontent_old.py
+++ src/Lawadata/getcontent_old.py
@@ -1,4 +1,4 @@
-print("Don't use this script")
+print("Don't Use this file. Its not used anymore")
 
 # from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
@@ -18,23 +18,20 @@ print("Don't use this script")
 # from selenium.webdriver.support import expected_conditions as EC
 # from selenium.common.exceptions import TimeoutException
 
-# """Important Notes:
-# You will need to have the Microsoft edge Webdriver installed (and added to PATH) before this will work.
-# I'm hoping that this will be easy to do on the virtual machine as well.
-# https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/?form=MA13LH
-# """
+
 
 # download_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 
-# def lawa_download_link_groundwater():
+# def lawa_download_link_groundwater() -> str:
 #     lawa_url = "https://www.lawa.org.nz/download-data/"
 #     """This Function goes to the lawa downloads page, clicks download, then graps the download link of the 
 #     confirm button (this makes sure we have the latest dataset). It returns a string url"""
 #     #Currently this is hardcoded to look for the Groundwater quality dataset on the lawa downloads page
 #     chrome_options = Options()
 #     chrome_options.add_argument("--disable-extensions")
+#     chrome_options.add_argument('--headless')
 #     download_dir = os.path.join(os.getcwd(), 'LawaDownloads')
 #     os.makedirs(download_dir, exist_ok=True)
 
@@ -53,34 +50,31 @@ print("Don't use this script")
 #     driver.find_element(By.XPATH, ground_water_xpath).click()
 #     confirm_xpath = """//*[@id="download-data-gwquality-dataset"]/div[1]/div/a"""
 #     element = WebDriverWait(driver, 10).until(
-#     EC.visibility_of_element_located((By.XPATH, confirm_xpath))).click()
-#     #element.find_element(By.XPATH, confirm_xpath).click()
-#     #exel_file_download_link = element.get_attribute('href')
+#     EC.visibility_of_element_located((By.XPATH, confirm_xpath)))
+#     element.find_element(By.XPATH, confirm_xpath)
+#     exel_file_download_link = element.get_attribute('href')
 #     #print(exel_file_download_link)
-#     time.sleep(100)
+    
 #     driver.quit()
-#     return True
+#     return exel_file_download_link
 
 
 
-# def download_excel_workbook(url, name="Groundwater"):
+# def download_excel_workbook(url, name="Groundwater") -> dict:
 #     """Function that takes the url download link of a file, and then returns a dictionary of worksheet:pandas df pairs"""
 #     print(f"Downloading content from {url}")
 #     response = requests.get(url)
 #     content_type = response.headers.get('Content-Type')
 #     assert 'excel' in content_type or url.endswith(('.xls', '.xlsx'))
-#     file_path = name + str(date.today()) + ".xlsx"
-#     with open(file_path, 'wb') as f: #Its safer to write the dataset to disk first, then read it into pandas
+#     file_dest = "LawaDownloads"
+#     os.makedirs(file_dest, exist_ok=True)
+
+#     save_path = os.path.join(file_dest, name + str(date.today()) + ".xlsx")
+#     with open(save_path, 'wb') as f: #Its safer to write the dataset to disk first, then read it into pandas
 #                 f.write(response.content)
 #     print("File written")
-#     dfs = pd.read_excel(file_path, sheet_name=None)
+#     print(save_path)
+#     dfs = pd.read_excel(save_path, sheet_name=None)
 
 #     return dfs
 
-
-# lawa_download_link_groundwater()
-
-# #dataframes = download_excel_workbook(link)
-
-# # for worksheet, df in dataframes.items():
-# #        print(df)
