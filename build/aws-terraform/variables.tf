7f386b17804cf0a3833bb8e318584a01776ca45f
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-28 11:13:42 +1200

    Updated main terraform script and added variables and versions tf files

--- build/aws-terraform/variables.tf
+++ build/aws-terraform/variables.tf
@@ -1,19 +1,60 @@
 variable "name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "DATA472-jre141-testserver"
+  default     = "DATA472-jcl173-testserver"
 }
 
 variable "course" {
   description = "The course name"
-  default     = "DATA472"
   type        = string
-
+  default     = "DATA472"
 }
 
 variable "username" {
   description = "The student or user name"
-  default     = "jre141"
   type        = string
+  default     = "jcl173"
+}
 
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "instance_type" {
+  description = "Machine type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "vpc_security_group_ids" {
+  description = "VPC Security group IDs"
+  type        = list(string)
+  default     = ["sg-09b486a14a006c62b"]
+}
+
+variable "instance_keypair_name" {
+  description = "Name of keypair"
+  type        = string
+  default     = "DATA472-jcl173-2"
+}
+
+
+variable "ami" {
+  description = "Amazon machine image"
+  type = string
+  default = "ami-080660c9757080771"
+}
+
+variable "subnetid" {
+  description = "The subnet id for machine"
+  type = string
+  default = "subnet-0326da008b859395c"
+}
+
+variable "volumesize" {
+  description = "Space on the volumn"
+  type = number
+  default = 20
 }
