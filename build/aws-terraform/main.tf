01d3b1c49c7ff7302f5c20a4afa7f09d54d5bbff
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-28 11:13:42 +1200

    Updated main terraform script and added variables and versions tf files

--- build/aws-terraform/main.tf
+++ build/aws-terraform/main.tf
@@ -1,25 +1,14 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region = "ap-southeast-2"
+  region = var.region
 }
 
-resource "aws_instance" "DATA472-jre141-ubuntu_instance" {
-  ami                         = "ami-080660c9757080771"
-  instance_type               = "t2.micro"
-  subnet_id                   = "subnet-0326da008b859395c"
-  vpc_security_group_ids      = ["sg-09b486a14a006c62b"]
+resource "aws_instance" "DATA472-jcl173-ubuntu_instance" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnetid
+  vpc_security_group_ids      = var.vpc_security_group_ids
   associate_public_ip_address = true
-  key_name                    = "DATA472-jre141-2"
+  key_name                    = var.instance_keypair_name
   ebs_optimized               = false
   tags = {
     Name     = var.name
@@ -28,7 +17,7 @@ resource "aws_instance" "DATA472-jre141-ubuntu_instance" {
   }
   ebs_block_device {
     device_name = "/dev/sda1"
-    volume_size = 20
+    volume_size = var.volumesize
   }
   volume_tags = {
     Name     = var.name
@@ -38,18 +27,3 @@ resource "aws_instance" "DATA472-jre141-ubuntu_instance" {
 
 
 }
-
-# resource "aws_ebs_volume" "ebs_volume" {
-#   availability_zone = "ap-southeast-2b"
-#   size              = 20
-#   type              = "gp3"
-#   iops              = 3000
-#   throughput        = 125
-#   encrypted         = false
-#   tags = {
-#     Name     = var.name
-#     Course   = var.course
-#     UserName = var.username
-#   }
-
-# }
