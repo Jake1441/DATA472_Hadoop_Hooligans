44463244b526e15733349deef879ea7852eab221
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-28 19:36:38 +1200

    Updated souring scripts to work in Docker, updated requirements

--- src/Lawadata/lawa_main.py
+++ src/Lawadata/lawa_main.py
@@ -1,16 +1,16 @@
-import download_workbook
-import get_groundwater_link
-from pyvirtualdisplay import Display
+from .download_workbook import download_excel_workbook
+from .get_groundwater_link import lawa_download_link_groundwater
+
 
 def get_lawa_groundwater() -> dict:
-    with Display(visable=False, size=(1280, 800)):
-        download_link = get_groundwater_link.lawa_download_link_groundwater()
-        data_dict = download_workbook.download_excel_workbook(download_link)
+    #with Display(visable=False, size=(1280, 800)):
+    download_link = lawa_download_link_groundwater()
+    data_dict = download_excel_workbook(download_link)
 
-        return data_dict
+    return data_dict
 
 
-if __name__ == "__main__":
-    data = get_groundwater_link()  #Example of how to access the dfs
-    for _, df in data.items():
-        print(df)
+# if __name__ == "__main__":
+#     data = get_groundwater_link()  #Example of how to access the dfs
+#     for _, df in data.items():
+#         print(df)
