7d3508afd6dbe9f649005f27011b6f39de90e1dd
Jacob1461 <35879589+Jacob1461@users.noreply.github.com>
2024-05-29 14:08:19 +1200

    Updated so Lawa no longer returns dictionary, only downloads files

--- src/source_data.py
+++ src/source_data.py
@@ -5,10 +5,10 @@ def source_data():
     """Main file for downloading the external data, Lawa data returns as a Dictionary of DF objects,
     Ecan Data is downloaded in the folder well_data"""
     print("Starting data sourcing")
-    lawa_data_dict = lawa_main.get_lawa_groundwater()
+    lawa_main.get_lawa_groundwater()
     ecanmain.create_water_reports()
     print("Finished data sourcing")
-    return lawa_data_dict
+    
 
 
 print(source_data())
