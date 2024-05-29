2205ce37ba2d2a0e05721eaf60aa8e8383f8c4df
Jacob Reid <jacob@sysint.co.nz>
2024-05-29 20:45:00 +1200

    updated ansible installer script

--- main.sh
+++ main.sh
@@ -3,7 +3,9 @@
 # build the required packages
 sh ./scripts/setup_ansible_mini.sh
 sh ./scripts/init_ansible.sh
+sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED /usr/lib/python3.12/EXTERNALLY-MANAGED.old
 pip install docker
+sudo mv /usr/lib/python3.12/EXTERNALLY-MANAGED.old /usr/lib/python3.12/EXTERNALLY-MANAGED
 
 cd ./build/docker_selenium/
 sh run_docker_scraper.sh
