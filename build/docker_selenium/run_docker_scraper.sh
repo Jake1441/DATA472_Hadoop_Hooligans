1e8502c84a6a08a651c054ffb9b97b3be47ca2ce
Your Name <you@example.com>
2024-05-30 02:32:49 +0000

    added logs for docker

--- build/docker_selenium/run_docker_scraper.sh
+++ build/docker_selenium/run_docker_scraper.sh
@@ -5,6 +5,8 @@
 #Test purpose go back to previous directory.
 # repo=desertroam/hadoophulligans
 # tag=latest
+log_file="./logs/$(date +'%Y-%d-%m')-pythonscraper.log"
 
-sudo docker compose up
+{ time ( sudo docker compose up 2>&1 | while IFS= read -r line; do printf '%s %s\n' "$(date)" "$line"; done ); } 2>&1 | tee -a "$log_file" | grep real >> "$log_file"
+
 #sudo docker run -it --name python-scraper $repo:$tag
