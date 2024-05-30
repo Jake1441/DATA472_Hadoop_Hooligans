c2beda56a7720ff67a51a4febb836361d58600c0
Jacob Reid <jacob@sysint.co.nz>
2024-05-30 14:07:11 +1200

    docker compose test

--- build/docker_selenium/run_docker_scraper.sh
+++ build/docker_selenium/run_docker_scraper.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
 # runs docker selenium scraper
-# fname is the file name.
-
-# name of the image is selenium-headless-d472
-# the docker container name is selcontainer
-
-#docker rmi selenium-headless-d472
-#docker build . -t selenium-headless-d472
-
-# test running
-# stage 1 - Ecan
 
 #Test purpose go back to previous directory.
-repo=desertroam/hadoophulligans
-tag=latest
+# repo=desertroam/hadoophulligans
+# tag=latest
 
-sudo docker run -it --name python-scraper $repo:$tag
+sudo docker compose up
+#sudo docker run -it --name python-scraper $repo:$tag
