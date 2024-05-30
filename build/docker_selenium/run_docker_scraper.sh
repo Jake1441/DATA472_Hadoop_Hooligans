1376bdd997dba9894950bf964cc16d5b65f1feef
Jacob Reid <jacob@sysint.co.nz>
2024-05-30 16:03:05 +1200

    code refactoring

--- build/docker_selenium/run_docker_scraper.sh
+++ build/docker_selenium/run_docker_scraper.sh
@@ -5,7 +5,8 @@
 #Test purpose go back to previous directory.
 # repo=desertroam/hadoophulligans
 # tag=latest
-log_file="./logs/$(date +'%Y-%d-%m')-pythonscraper.log"
+log_file="../logs/$(date +'%Y-%d-%m')-pythonscraper.log"
+docker volume create scraper-db
 
 { time ( sudo docker compose up 2>&1 | while IFS= read -r line; do printf '%s %s\n' "$(date)" "$line"; done ); } 2>&1 | tee -a "$log_file" | grep real >> "$log_file"
 
