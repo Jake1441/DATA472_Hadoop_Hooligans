5dbc94aa92355ffb0966db5a8421a5efbbcd6ef9
Naman Rastogi <namanrastogi.materiox@gmail.com>
2024-06-01 07:54:51 +0000

    Adds Acceptable Determinands

--- src/insert_into_acceptable_determinands.py
+++ src/insert_into_acceptable_determinands.py
@@ -1,7 +1,35 @@
 import pandas as pd
+import connect_to_db as connect
+import json
+
+def get_measurement_value(df, measurement):
+    row = df[df['Measurement'] == measurement]
+    if not row.empty:
+        determinand_min_value = row.iloc[0]['Min Value']
+        determinand_ideal_value = row.iloc[0]['Ideal Value']
+        determinand_max_value = row.iloc[0]['Max Value']
+        units = row.iloc[0]['Units']
+        formula = row.iloc[0]['Formula']
+
+        return {"determinand_name": str(measurement),
+                "determinand_units": str(units),
+                "determinand_formula": str(formula),
+                "determinand_min_value": str(determinand_min_value),
+                "determinand_ideal_value": str(determinand_ideal_value),
+                "determinand_max_value": str(determinand_max_value),
+                }
+    else:
+        return {"determinand_name": None,
+                "determinand_units": None,
+                "determinand_formula": None,
+                "determinand_min_value": None,
+                "determinand_ideal_value": None,
+                "determinand_max_value": None,
+                }
 
 
-def insert_into_acceptable_determinands(connection, row):
+def insert_into_acceptable_determinands(connection, df):
+    cur = connection.cursor()
     try:
         cur = connection.cursor()
         insert_into_command = """
@@ -24,41 +52,64 @@ def insert_into_acceptable_determinands(connection, row):
         )
         VALUES
         (
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s,
-         %s
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb,
+         %s::jsonb
         );
         """
+        chloride = get_measurement_value(df, 'Chloride')
+        hardness_total = get_measurement_value(df, 'Hardness, Total')
+        calcium = get_measurement_value(df, 'Calcium, Dissolved')
+        ph = get_measurement_value(df, 'pH')
+        ph_field = get_measurement_value(df, 'pH (Field)')
+        sodium_dissolved = get_measurement_value(df, 'Sodium, Dissolved')
+        sulphate = get_measurement_value(df, 'Sulphate')
+        water_temperature = get_measurement_value(df, 'Water Temperature (Field)')
+        oxygen_dissolved = get_measurement_value(df, 'Dissolved Oxygen')
+        magnesium = get_measurement_value(df, 'Magnesium, Dissolved')
+        silica = get_measurement_value(df, 'Silica, Reactive')
+        nitrate_nitrogen = get_measurement_value(df, 'Nitrate Nitrogen')
+        e_coli = get_measurement_value(df, 'E. coli')
+        total_coliforms = get_measurement_value(df, 'Total Coliforms')
         cur.execute(
             insert_into_command,
             (
-                row["Well_No"],
-                row["Well_Status"],
-                row["Well_Type"],
-                row["Well_Owner"],
-                row["Driller"],
-                row["Primary_Use"],
-                row["Secondary_Use"],
-                row["WGS84_LATITUDE"],
-                row["WGS84_LONGITUDE"],
-                row["Link"],
-                row["Locality"],
-                row["Street"],
+                json.dumps(chloride),
+                json.dumps(hardness_total),
+                json.dumps(calcium),
+                json.dumps(ph),
+                json.dumps(ph_field),
+                json.dumps(sodium_dissolved),
+                json.dumps(sulphate),
+                json.dumps(water_temperature),
+                json.dumps(oxygen_dissolved),
+                json.dumps(magnesium),
+                json.dumps(silica),
+                json.dumps(nitrate_nitrogen),
+                json.dumps(e_coli),
+                json.dumps(total_coliforms)
             ),
         )
         connection.commit()
         print("Data Inserted")
         cur.close()
     except Exception as e:
-        print(f"Exception occured at {row['Well_No']} {e}")
+        print(f"Exception occured {e}")
+
+
+
+if __name__ == "__main__":
+    acc_df = pd.read_csv('acc_deter.csv')
+    connection = connect.connect_to_database()
+    insert_into_acceptable_determinands(connection, acc_df)
