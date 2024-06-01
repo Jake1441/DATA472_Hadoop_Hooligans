258121917ab25a8493a5a2c005cac6f0fbc1491b
JulianM <jjm148@uclive.ac.nz>
2024-06-02 10:07:48 +1200

    updated rebuild_docker

--- scripts/rebuild_docker.sh
+++ scripts/rebuild_docker.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-dockerbuild='/home/ubuntu/DATA472_Hadoop_Hooligans'
-dockercompose='/home/ubuntu/DATA472_Hadoop_Hooligans/build/docker-scraper'
-dockerimg='desetroam/hadoophulligans:latest'
-
-cd $dockerbuild
-docker build . --no-cache -t $dockerimg
-
-cd $dockercompose
-bash start_scraper.sh
-
-log_file="../../logs/$(date +'%Y-%d-%m-%H:%M')-docker-scraper.log"
-scriptfile='rebuild_docker.sh' # should be script name 
-
-echo "Please check $log_file to view any activities"
-
 mc_details=("$(uname)" "$(lsb_release -c | awk '{print $2}')")
 cat << EOF >> "$log_file"
 $(printf "%0.s-" {1..32})
@@ -28,6 +13,24 @@ you appear to be running on ${mc_details[0]}\\${mc_details[1]}
 $(printf "%0.s-" {1..32})
 
 EOF
+
+dockerbuild='/home/ubuntu/DATA472_Hadoop_Hooligans'
+dockercompose='/home/ubuntu/DATA472_Hadoop_Hooligans/build/docker-scraper'
+dockerimg='desetroam/hadoophulligans:latest'
+
+cd $dockerbuild
+
+{ time ( docker build . --no-cache -t $dockerimg | while IFS= read -r line; do printf '%s %s %s %s %s\n' "$HOSTNAME:" "$(whoami)" - "$(date)" "$line"; done ); } 2>&1 | tee -a "$log_file" | grep real >> "$log_file"
+# note the {time (cmd plus args | while IFS= read -r line; do printf '%s %s %s %s %s\n' "$HOSTNAME:" "$(whoami)" - "$(date)" "$line"; done ); } 2>&1 | tee -a "$log_file" | grep real >> "$log_file")}
+# Thats the part that outputs to the logs :) Eventually we can use an environment variable to make it easier.
+
+cd $dockercompose
+bash start_scraper.sh
+
+log_file="../../logs/$(date +'%Y-%d-%m-%H:%M')-docker-scraper.log"
+scriptfile='rebuild_docker.sh' # should be script name 
+
+echo "Please check $log_file to view any activities"
 
 
 echo "running $scriptfile" >> $log_file
