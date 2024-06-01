b808b06bd30d800bee66aceb36ae6390834ce13b
JulianM <jjm148@uclive.ac.nz>
2024-06-02 10:03:16 +1200

    updated find_rename.sh

--- scripts/find_rename.sh
+++ scripts/find_rename.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-find . -type f -not -path '*/\.git/*' -exec grep -l 'run_docker_scraper.sh' {} \; -exec sed -i 's/run_docker_scraper.sh/start_scraper.sh/g' {} \;
-
-log_file="../../logs/$(date +'%Y-%d-%m-%H:%M')-pythonscraper.log"
-scriptfile='find_rename.sh' # should be script name 
-
-echo "Please check $log_file to view any activities"
-
 mc_details=("$(uname)" "$(lsb_release -c | awk '{print $2}')")
 cat << EOF >> "$log_file"
 $(printf "%0.s-" {1..32})
@@ -20,6 +13,13 @@ you appear to be running on ${mc_details[0]}\\${mc_details[1]}
 $(printf "%0.s-" {1..32})
 
 EOF
+
+find . -type f -not -path '*/\.git/*' -exec grep -l 'run_docker_scraper.sh' {} \; -exec sed -i 's/run_docker_scraper.sh/start_scraper.sh/g' {} \;
+
+log_file="../../logs/$(date +'%Y-%d-%m-%H:%M')-pythonscraper.log"
+scriptfile='find_rename.sh' # should be script name 
+
+echo "Please check $log_file to view any activities"
 
 
 echo "running $scriptfile" >> $log_file
